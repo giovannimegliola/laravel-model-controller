214217afe61866826b4e721e61a6f5829b83cd05
giovannimegliola <giovanni.megliola@hotmail.it>
2023-12-20 16:32:20 +0100

    printed movies cards

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,15 +1,27 @@
 @extends('layouts.app')
 
-@section('title', 'All Movies')
+@section('title', 'Movies')
 
 @section('content')
 
-<ul>
-
-@foreach ($movies as $movie)
-<li><a href="{{route('movies.show', $movie->id )}}"> {{$movie->title}}</a></li>
-
-@endforeach
-</ul>
+<main>
+    <section class="container">
+        <h1>Movies</h1>
+        <div class="row gy-4">
+          @foreach ($movies as $key => $movie)
+            <div class="col-12 col-md-4 col-lg-3">
+                <div class="card">
+                    <img src="{{$movie['image']}}" alt="{{$movie['title']}}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$movie['title']}}</h5>
+                        <p class="card-text">{{$movie['original_title']}}</p>
+                        <a href="{{route('movies.show', $key)}}" class="btn btn-primary">Vedi dettagli</a>
+                    </div>
+                </div>
+            </div>
+          @endforeach
+        </div>
+    </section>
+</main>
 
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,33 @@
-<div>Footer</div>
+<footer class="bg-white mt-5 pt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h4 class="text-uppercase">Lorem ipsum dolor</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#">Lorem.</a></li>
+                    <li><a href="#">Quas.</a></li>
+                    <li><a href="#">Volupt</a></li>
+                    <li><a href="#">Esse.</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4 class="text-uppercase">Quick links</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#">Lorem.</a></li>
+                    <li><a href="#">Quas.</a></li>
+                    <li><a href="#">Volupt</a></li>
+                    <li><a href="#">Esse.</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4 class="text-uppercase">Privacy Policy</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#">Lorem.</a></li>
+                    <li><a href="#">Quas.</a></li>
+                    <li><a href="#">Volupt</a></li>
+                    <li><a href="#">Esse.</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
