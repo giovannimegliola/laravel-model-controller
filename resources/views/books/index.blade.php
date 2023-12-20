503302361d317ebc0bf1c90a4c16acec5ab73aa3
giovannimegliola <giovanni.megliola@hotmail.it>
2023-12-20 16:42:18 +0100

    printed books cards

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,15 +1,28 @@
 @extends('layouts.app')
 
-@section('title', 'All Books')
+@section('title', 'Books')
 
 @section('content')
 
-<ul>
-
-@foreach ($books as $book)
-<li><a href="{{route('books.show', $book->id )}}">{{$book->title}}</a></li>
-
-@endforeach
-</ul>
+<main>
+    <section class="container">
+        <h1>Books</h1>
+        <div class="row gy-4">
+          @foreach ($books as $key => $book)
+            <div class="col-12 col-md-4 col-lg-3">
+                <div class="card">
+                    <img src="{{$book['cover_image']}}" alt="{{$book['title']}}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$book['title']}}</h5>
+                        <p class="card-text">{!! substr($book['plot'], 0, 100) . '...' !!}</p>
+                        <a href="{{route('books.show', $key)}}" class="btn btn-primary">Vedi dettagli</a>
+                    </div>
+                </div>
+            </div>
+          @endforeach
+        </div>
+    </section>
+</main>
 
 @endsection
+
