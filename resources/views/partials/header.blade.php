ba94362c9281427d02de7d25fac19b7669cfa517
giovannimegliola <giovanni.megliola@hotmail.it>
2023-12-20 16:16:54 +0100

    added navbar in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,36 @@
-<div>Header <i class="fa-solid fa-home"></i></div>
+<header>
+
+    <div>
+
+        <nav class="navbar-nav container navbar-light">
+            <ul class="list-unstyled d-flex justify-content-center gap-2 text-uppercase">
+
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::currentRouteName() == 'home' ? 'active' : '' }}"
+                        href="{{ route('home') }}">
+                        Home
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link {{ Route::currentRouteName() == 'movies.index' ? 'active' : '' }}"
+                        href="{{ route('movies.index') }}">
+                        Movies
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::currentRouteName() == 'books.index' ? 'active' : '' }}"
+                        href="{{ route('books.index') }}">
+                        Books
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+
+    </div>
+
+</header>
+
+
